32b5f68fcc2bc2fa981b0d920a80f93d40542e6a
lshyun955 <lshyun955@gmail.com>
2022-05-25 15:27:10 +0900

    Fix: Router.js 경로 및 컴포넌트명 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../pages/suhyeon/Login/Login";
-import Main from "../pages/suhyeon/Main/Main";
+import SuhyeonLogin from "../pages/suhyeon/Login/Login";
+import SuhyeonMain from "../pages/suhyeon/Main/Main";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/main" element={<Main />} />
+        <Route path="/suhyeonLogin" element={<SuhyeonLogin />} />
+        <Route path="/suhyeonMain" element={<SuhyeonMain />} />
       </Routes>
     </BrowserRouter>
   );
