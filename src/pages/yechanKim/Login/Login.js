d782df0f8bad16a8743671edca6f20bb236128dc
developer-yechan <kyc5244@gmail.com>
2022-05-26 16:39:16 +0900

    로그인 버튼 활성화 메인 댓글 추가

--- src/pages/yechanKim/Login/Login.js
+++ src/pages/yechanKim/Login/Login.js
@@ -1,23 +1,60 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.scss";
-import { useState } from "react";
 
 function Login() {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const navigate = useNavigate();
+
+  function handleIdInput(e) {
+    setId(e.target.value);
+    validation(e.target.value, pw);
+  }
+  function handlePwInput(e) {
+    setPw(e.target.value);
+    validation(id, e.target.value);
+  }
+  function validation(id, pw) {
+    if (id.indexOf("@") !== -1 && pw.length >= 5) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const valid = validation(id, pw);
   return (
     <div className="login-container">
       <div className="login">
         <h1>Instagram</h1>
         <div className="login-form">
           <input
+            value={id}
+            onChange={handleIdInput}
             type="text"
             id="id"
             className="info"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
-          <input type="text" id="pw" className="info" placeholder="비밀번호" />
-          <button id="login-button" className="login-button">
-            <Link to="/main">로그인</Link>
+          <input
+            value={pw}
+            onChange={handlePwInput}
+            type="password"
+            id="pw"
+            className="info"
+            placeholder="비밀번호"
+          />
+          <div>{id}</div>
+          <button
+            id="login-button"
+            disabled={!valid}
+            className={valid ? "login-button-active" : "login-button-inactive"}
+            onClick={() => {
+              navigate("/main");
+            }}
+          >
+            로그인
           </button>
         </div>
         <a href="#" className="missing-pw">
--- src/pages/yechanKim/Main/Main.js
+++ src/pages/yechanKim/Main/Main.js
@@ -2,11 +2,38 @@ import React, { useState } from "react";
 import "./Main.scss";
 
 function Main() {
+  const [comment, setComment] = useState("");
+  const commentArr = [];
+  const commentWrap = document.createElement("ul");
+
+  function makeComment() {
+    const commentInput = document.getElementById("comment");
+    const commentBox = document.getElementsByClassName("display-comment")[0];
+    const commentHtml = `<li><a href=""><b>yechan</b></a>
+  <span class="text">${comment}</span>
+  <span class="material-symbols-outlined">favorite</span></li>`;
+    commentArr.push(commentHtml);
+    const sumComment = commentArr.join("");
+    commentWrap.innerHTML = sumComment;
+    commentBox.appendChild(commentWrap);
+    commentInput.value = "";
+  }
+  function saveComment(e) {
+    if (e.key === "Enter") {
+      if (comment.length >= 1) {
+        makeComment();
+      }
+    } else if (e.target.tagName === "BUTTON") {
+      if (comment.length >= 1) {
+        makeComment();
+      }
+    }
+  }
   return (
     <div>
       <nav>
         <div className="logo-title">
-          <img src="/images/instagram.png" alt="instagram" />
+          <img src="/images/yechanKim/instagram.png" alt="instagram" />
           <h1>Instagram</h1>
         </div>
         <div className="search">
@@ -25,12 +52,16 @@ function Main() {
           <div className="post">
             <header className="edit-group">
               <div className="editor">
-                <img className="profile-img" src="/images/me.jpg" alt="" />
+                <img
+                  className="profile-img"
+                  src="/images/yechanKim/me.jpg"
+                  alt=""
+                />
                 <span className="'nickname">yechan</span>
               </div>
               <span className="material-symbols-outlined">more_horiz</span>
             </header>
-            <img src="/images/character.png" alt="" />
+            <img src="/images/yechanKim/character.png" alt="" />
             <div className="contents-info">
               <div className="icon-group">
                 <span className="material-symbols-outlined">favorite</span>
@@ -39,7 +70,11 @@ function Main() {
                 <span className="material-symbols-outlined">bookmark</span>
               </div>
               <div className="like">
-                <img className="profile-img" src="/images/me.jpg" alt="" />
+                <img
+                  className="profile-img"
+                  src="/images/yechanKim/me.jpg"
+                  alt=""
+                />
                 <span className="'like-message">
                   <b>yechan</b>님 외 <b>10명</b>이 좋아합니다.
                 </span>
@@ -54,16 +89,32 @@ function Main() {
               <span className="info">42분 전</span>
             </div>
             <div className="leave-comment">
-              <input id="comment" type="text" placeholder="댓글 달기..." />
-              <strong id="post" className="blue">
+              <input
+                onChange={(e) => {
+                  setComment(e.target.value);
+                }}
+                onKeyPress={(e) => saveComment(e)}
+                id="comment"
+                type="text"
+                placeholder="댓글 달기..."
+              />
+              <button
+                id="post"
+                className="blue"
+                onClick={(e) => saveComment(e)}
+              >
                 게시
-              </strong>
+              </button>
             </div>
           </div>
         </main>
         <aside>
           <div className="my-account">
-            <img className="profile-img" src="/images/me.jpg" alt="" />
+            <img
+              className="profile-img"
+              src="/images/yechanKim/me.jpg"
+              alt=""
+            />
             <div className="profile-info">
               <strong className="id">yechan</strong>
               <span className="info">김예찬</span>
@@ -75,7 +126,11 @@ function Main() {
               <strong>모두 보기</strong>
             </div>
             <div className="list">
-              <img className="profile-img" src="/images/me.jpg" alt="" />
+              <img
+                className="profile-img"
+                src="/images/yechanKim/me.jpg"
+                alt=""
+              />
               <div className="profile-info">
                 <strong className="id">yechan</strong>
                 <span className="info">10분전</span>
@@ -88,7 +143,11 @@ function Main() {
               <strong>모두 보기</strong>
             </div>
             <div className="list">
-              <img className="profile-img" src="/images/me.jpg" alt="" />
+              <img
+                className="profile-img"
+                src="/images/yechanKim/me.jpg"
+                alt=""
+              />
               <div className="profile-info">
                 <strong className="id">yechan</strong>
                 <span className="info">_legend_님 외 1명이 ...</span>
