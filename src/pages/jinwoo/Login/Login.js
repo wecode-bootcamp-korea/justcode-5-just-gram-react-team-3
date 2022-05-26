14bdf50cb3f9f4c0c6d9e14678057bd448d3b99f
happyeveryone96 <happyeveryone@naver.com>
2022-05-26 15:48:25 +0900

    Refactor: prettier 적용

--- src/pages/jinwoo/Login/Login.js
+++ src/pages/jinwoo/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 import './Login.scss';
 
@@ -14,39 +14,55 @@ function Login() {
     } else {
       setIsValid(false);
     }
-  }
+  };
 
   const handleIdInput = (e) => {
     setIdValue(e.target.value);
-  }
+  };
 
   const handlePwInput = (e) => {
     setPwValue(e.target.value);
-  }
+  };
 
   const navigate = useNavigate();
 
   const goToMain = () => {
-    navigate("/jinwooMain");
+    navigate('/jinwooMain');
   };
   return (
-      <div className="container">
-        <h1>Justgram</h1>
-        <div className="login-form">
-          <input type="text" id="id-input" onKeyUp={updateBtn} onChange={handleIdInput} placeholder="전화번호, 사용자 이름 또는 이메일"/>
-          <input type="password" id="pw-input" onKeyUp={updateBtn} onChange={handlePwInput} placeholder="비밀번호"/>
-          <button 
-            disabled={isValid ? false : true} 
-            style={{
-              backgroundColor: isValid ? 'rgb(115,180,226)': 'rgb(172,212,234)', 
-              cursor: isValid ? 'pointer' : 'auto'}} 
-            onClick={goToMain}>
-              로그인
-          </button>
-        </div>
-        <a href="/" id="search-pw">비밀번호를 잊으셨나요?</a>
+    <div className="container">
+      <h1>Justgram</h1>
+      <div className="login-form">
+        <input
+          type="text"
+          id="id-input"
+          onKeyUp={updateBtn}
+          onChange={handleIdInput}
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+        <input
+          type="password"
+          id="pw-input"
+          onKeyUp={updateBtn}
+          onChange={handlePwInput}
+          placeholder="비밀번호"
+        />
+        <button
+          disabled={isValid ? false : true}
+          style={{
+            backgroundColor: isValid ? 'rgb(115,180,226)' : 'rgb(172,212,234)',
+            cursor: isValid ? 'pointer' : 'auto',
+          }}
+          onClick={goToMain}
+        >
+          로그인
+        </button>
       </div>
-  )
+      <a href="/" id="search-pw">
+        비밀번호를 잊으셨나요?
+      </a>
+    </div>
+  );
 }
 
 export default Login;
