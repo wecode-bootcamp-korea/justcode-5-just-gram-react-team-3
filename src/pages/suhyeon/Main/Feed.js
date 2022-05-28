d31477a5a59630541e9fe28c4f6c8d7e53119e12
lshyun955 <lshyun955@gmail.com>
2022-05-28 21:43:32 +0900

    Add: 댓글 작성 기능 구현

--- src/pages/suhyeon/Main/Feed.js
+++ src/pages/suhyeon/Main/Feed.js
@@ -1,74 +1,140 @@
 import Comment from "./Comment";
+import heartImage from "../assets/heart.png";
+import pinkheartImage from "../assets/pinkHeart.png";
+import { useState } from "react";
+function Feed({
+  author,
+  content,
+  date,
+  comments,
+  imageUrl,
+  likePeople,
+  profileImageUrl,
+  isLiked,
+}) {
+  const [commentArr, setCommentArr] = useState(comments);
+  const [comment, setComment] = useState("");
+  const [isLike, setIsLike] = useState(isLiked);
 
-function Feed({ enterCheck, valueCheck, writeComment, feedList, comment }) {
-  const feeds = feedList.map((v) => (
-    <article key={v.feedId} className="feed_container">
+  const changeHeart = () => {
+    if (isLike === true) {
+      setIsLike(false);
+    } else {
+      setIsLike(true);
+    }
+  };
+
+  const enterCheck = (e) => {
+    if (e.key === "Enter") {
+      if (e.target.value !== "") {
+        writeComment();
+      }
+    }
+  };
+
+  const valueCheck = (e) => {
+    setComment(e.target.value);
+  };
+
+  const writeComment = () => {
+    let temp = [
+      ...commentArr,
+      {
+        id: commentArr.length + 1,
+        author: "lshyun955",
+        content: comment,
+        isLiked: false,
+      },
+    ];
+    console.log(temp);
+    setCommentArr(temp);
+    setComment("");
+  };
+
+  return (
+    <article className="feed_container">
       <section className="feed_header">
         <section className="feed_writer">
-          <img className="feed_writer_photo" alt="" src={v.author.imageUrl} />
-          <a href="/">{v.author.nickname}</a>
+          <img className="feed_writer_photo" alt="" src={profileImageUrl} />
+          <a href="/suHyeonMain">{author}</a>
         </section>
         <button className="feed_btn">
-          <img src="/images/suhyeon/more.png" width="20px" />
+          <img src="/images/suhyeon/more.png" alt="more" width="20px" />
         </button>
       </section>
-      <img alt="aa" className="feed_photo" src={v.imageUrl} />
+      <img alt="aa" className="feed_photo" src={imageUrl} />
       <section className="feed_content">
         <section className="feed_button_group">
           <section className="like_comment_share">
-            <button className="feed_btn">
-              <img src="/images/suhyeon/heart.png" width="15px" />
+            <button className="feed_btn" onClick={changeHeart}>
+              <img
+                src={isLike ? pinkheartImage : heartImage}
+                alt="heart"
+                width="15px"
+              />
             </button>
             <button className="feed_btn">
-              <img src="/images/suhyeon/speech-bubble.png" width="15px" />
+              <img
+                src="/images/suhyeon/speech-bubble.png"
+                alt="speech-bubble"
+                width="15px"
+              />
             </button>
             <button className="feed_btn">
-              <img src="/images/suhyeon/share.png" width="15px" />
+              <img src="/images/suhyeon/share.png" alt="share" width="15px" />
             </button>
           </section>
           <section className="bookmark">
             <button className="feed_btn">
-              <img src="/images/suhyeon/bookmark-white.png" width="15px" />
+              <img
+                src="/images/suhyeon/bookmark-white.png"
+                alt="bookmark"
+                width="15px"
+              />
             </button>
           </section>
         </section>
         <section className="like_list_box">
           <div className="like_feed_user">
-            <img alt="aa" className="like_photo" src={v.likePeople.imageUrl} />
-            <a href="/">{v.likePeople.nickname}</a>님&nbsp;
-            <span className="bold">외&nbsp;{v.likePeople.count}명</span>이&nbsp;
-            좋아합니다.
+            <img alt="aa" className="like_photo" src={likePeople.imageUrl} />
+            <a href="/suHyeonMain">{likePeople.nickname}</a>님&nbsp;
+            <span className="bold">외&nbsp;{likePeople.count}명</span>
+            이&nbsp; 좋아합니다.
           </div>
         </section>
         <div className="main_content">
-          <a href="/">{v.author.nickname}</a>
-          <span className="each_feed_content">{v.content}</span>
+          <a href="/suHyeonMain">{author}</a>
+          <span className="each_feed_content">{content}</span>
+          <div className="feed_date">작성일:{date}</div>
         </div>
         <section className="feed_comment">
-          <Comment commentList={v.comments} />
+          {commentArr.map((comment) => {
+            return (
+              <Comment
+                key={comment.id}
+                author={comment.author}
+                content={comment.content}
+                isLiked={comment.isLiked}
+              />
+            );
+          })}
         </section>
         <section className="comment_box">
           <input
-            onKeyDown={(e) => enterCheck(e)}
-            onChange={(e) => valueCheck(e)}
+            onKeyUp={enterCheck}
+            onChange={valueCheck}
             value={comment}
             className="write_comment"
             type="text"
             placeholder="댓글 달기"
           />
-          <button
-            onClick={() => {
-              writeComment("lshyun955", comment);
-            }}
-            className="write_comment_btn"
-          >
+          <button onClick={writeComment} className="write_comment_btn">
             게시
           </button>
         </section>
       </section>
     </article>
-  ));
-  return <>{feeds}</>;
+  );
 }
 
 export default Feed;
