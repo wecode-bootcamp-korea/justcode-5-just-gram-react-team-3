026de6423086f348b7f25a8f8bac680459cc0f9a
happyeveryone96 <happyeveryone@naver.com>
2022-05-30 20:09:08 +0900

    Add: 피드별 댓글 삭제 기능 추가

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Comment.scss';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -7,22 +7,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 library.add(faHeart);
 
 function Comment(props) {
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const delComment = () => {
+    setIsDeleted(true);
+  };
+
   return (
     <>
-      <div className="userComment">
-        <div className="comment">
-          <span className="userName">{props.userName}</span>
-          <span> {props.content}</span>
+      {isDeleted ? (
+        <></>
+      ) : (
+        <div className="userComment">
+          <div className="comment">
+            <span className="userName">{props.userName}</span>
+            <span> {props.content}</span>
+          </div>
+          <div className="commentRight">
+            <FontAwesomeIcon
+              icon="fa-solid fa-heart"
+              color="gray"
+              className="heart"
+            />
+            <button className="deleteCommentBtn" onClick={delComment}>
+              삭제
+            </button>
+          </div>
         </div>
-        <div className="commentRight">
-          <FontAwesomeIcon
-            icon="fa-solid fa-heart"
-            color="gray"
-            className="heart"
-          />
-          <button className="deleteCommentBtn">삭제</button>
-        </div>
-      </div>
+      )}
     </>
   );
 }
