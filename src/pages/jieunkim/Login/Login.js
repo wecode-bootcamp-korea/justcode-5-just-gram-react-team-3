b04dc2dd83b2465c469dfbfbe8e433a6e68aace9
Jieun Kim <jieun.k@hotmail.com>
2022-05-31 00:51:13 -0700

    Add: signup and login using fetch

--- src/pages/jieunkim/Login/Login.js
+++ src/pages/jieunkim/Login/Login.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.scss";
 
+
+
 function Login() {
+  const signupAPI = "http://52.79.143.176:8000/users/signup";
+  const loginAPI = "http://52.79.143.176:8000/users/login";
   const [userId, setUserId] = useState("");
   const [userPassword, setPassword] = useState("");
 
@@ -12,14 +16,77 @@ function Login() {
     return (userId.includes("@") && userPassword.length > 7);
   }
 
+  // const buttonOnclick = () => {
+  //   if (isActiveLoginBtn()) {
+  //     navigate("/main",
+  //     {state: userId});
+  //   } else {
+  //     navigate("/");
+  //   }
+  // }
+
   const buttonOnclick = () => {
     if (isActiveLoginBtn()) {
-      navigate("/main",
-      {state: userId});
-    } else {
-      navigate("/");
+      fetch(loginAPI, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: userId,
+          password: userPassword,
+        }),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log("결과: ", result)
+          let msg = result.message;
+          if (msg.includes("SUCCESS")) {
+            navigate("/main",
+            {state: userId});
+          } else if (msg.includes('client input invalid')) {
+            alert("please check your id and password")
+            navigate("/");
+          } else {
+            navigate("/");
+          }
+        });
     }
   }
+
+  const buttonOnclickSignup = () => {
+    if (isActiveLoginBtn()) {
+      fetch(signupAPI, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: userId,
+          password: userPassword,
+        }),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log("결과: ", result)
+          let msg = result.message;
+          if (msg.includes("SUCCESS")) {
+            navigate("/main",
+            {state: userId});
+          } else if (msg.includes('EXISTING_USER')) {
+            alert("SignUp failed. id exist")
+            navigate("/");
+          } else if (msg.includes('INVALID_PASSWORD')) {
+            alert('SignUp failed. Over 8 letter…ain capital letters, numbers, and special letters');
+            navigate("/");
+          } else {
+            alert('SignUp failed.')
+            navigate("/");
+          }
+        });
+    }
+  }
+
 
   return (
     <div className="Login">
